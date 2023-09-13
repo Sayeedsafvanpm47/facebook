35c79017e46ae474a202168817fcfeaa54168545
sayeed safvan <sayeedsafvan123@gmail.com>
2023-09-13 17:54:10 +0530

    form

---
+++ main.js
@@ -0,0 +1,33 @@
+function auth(){
+          var email = document.getElementById('email').value;
+          var password = document.getElementById('password').value;
+          
+          
+          
+          if(email.length!=0 && password.length!=0)
+          {
+                    window.location.assign("feed.html");
+                    alert("Succesfully logged in");
+          }
+}
+
+
+
+
+$("#submit-form").submit((e)=>{
+          e.preventDefault()
+          $.ajax({
+              url:"https://script.google.com/macros/s/AKfycby-CN-x8xsGnf23yAnboFuWzMulV0G_6EWFwHRJ-GLtvTcC71tpkr7DK_Otk__oEQBW/exec",
+              data:$("#submit-form").serialize(),
+              method:"post",
+              success:function (response){
+                  alert("Your account has been created successfully")
+                  window.location.reload()
+                  //window.location.href="https://google.com"
+              },
+              error:function (err){
+                  alert("Something Error")
+  
+              }
+          })
+      })
